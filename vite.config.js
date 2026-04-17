00a34c810054a2b3d1f2cb2305a8c6cfdc28c8bb
Alphapolygon <janne.piekkola@gmail.com>
2026-04-17 10:03:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-  },
-});
+  plugins: [react()],
+  // Add this line. Make sure it matches your GitHub repo name exactly!
+  base: '/circuit-companion/', 
+})
